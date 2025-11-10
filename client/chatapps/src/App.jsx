1968b95102aa93395dc321f7dde2a3d06112793c
budhathoki10 <budhathokikushal170@gmail.com>
2025-11-10 23:31:20 +0545

    all

--- client/chatapps/src/App.jsx
+++ client/chatapps/src/App.jsx
@@ -1,19 +1,45 @@
 import React, { useEffect } from 'react'
+import { useMemo } from 'react';
+import { useState } from 'react';
 import io from 'socket.io-client'
 
 const App = () => {
-  const socket = io("http://localhost:5000");
+  const [message, setMessage]= useState('')
+  const [id,setid]=useState("")
+  const [room, setRoom]=useState("")
+  const [getmessages,setgetMessage]=useState([])
+  const [group,setgroup]=useState()
+  const socket = useMemo(()=>io("http://localhost:5000"),[]);
+const handleform= (e)=>{
+  e.preventDefault();
+socket.emit("message",{message,room,group})
+setMessage('')
+}
+const joinroomhandler=(e)=>{
+e.preventDefault()
+socket.emit("join-room",group)
+console.log(`user joined in a room ${group}`)
+setgroup("")
 
-  useEffect(() => {
+}
 
+useEffect(() => {
+  
+console.log(getmessages)
     socket.on("connect", () => {
       console.log("Connected to server with ID:", socket.id);
+      setid(socket.id)
     });
 
 
     socket.on("welcome", (message) => {
       console.log(message);
     });
+ socket.on("recieve-message",(data)=>{
+        console.log(data)
+        setgetMessage((getmessages)=>[...getmessages,data])
+      })
+    
 
     return () => {
       socket.disconnect();
@@ -21,7 +47,37 @@ const App = () => {
   }, []); 
 
   return (
-    <div>App</div>
+   <>
+  <p>room ID: </p>
+   {
+    id
+   }
+   <form action="" onSubmit={handleform}>
+  
+    <div>
+      <input  onChange= {(e=>setMessage(e.target.value))} value={message} type="text"  placeholder='your message'/>
+<input  onChange= {(e=>setRoom(e.target.value))}  type="text"  placeholder='Room ID'/>
+      <button type='submit'>send</button><br />
+      
+    </div>
+   </form>
+
+   <form action="" onSubmit={joinroomhandler}>
+    <input value={group} onChange= {(e=>setgroup(e.target.value))}  type="text"  placeholder='group name'/>
+      <button type='submit'>join</button>
+   </form>
+
+<div style={{ marginTop: '20px' }}>
+        <h3>Messages:</h3>
+        {getmessages.map((msg, index) => (
+          <p key={index}>{msg.message || msg}</p>
+        ))}
+      </div>
+
+
+   
+   
+   </>
   )
 }
 
